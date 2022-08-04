fac00ba74fbb8ee41f3a2a07ebd155261649b10c
Edwin Carl Flores <edwincarlflores@gmail.com>
2022-08-04 18:37:49 +0800

    Added tailwind typography plugin. Created custom _document

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["'Montserrat'"],
+      mono: ["'Inconsolata'"],
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
